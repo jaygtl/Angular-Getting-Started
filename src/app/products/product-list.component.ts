6de251d8d81fa0a266c435123bd7f61ad42f4494
Jay Rao <jay.rao@gatewaytechnolabs.com>
2019-08-20 22:30:56 +0200

    added routing and canactivate

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
   selector: "pm-products",
@@ -7,11 +8,15 @@ import { IProduct } from './product';
   styleUrls: ["./product-list.component.css"]
 })
 export class ProductListComponent implements OnInit {
+
+  constructor(private productService: ProductService) {
+  }
   pageTitle = "Product List";
   imageWidth = 50;
   imageMargin = 2;
   showImage = false;
-  _listFilter:string;
+  _listFilter: string;
+  errorMessage: string;
   get listFilter(): string {
     return this._listFilter;
   }
@@ -20,43 +25,29 @@ export class ProductListComponent implements OnInit {
     this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
   }
   filteredProducts: IProduct[];
-  products: IProduct[] = [
-    {
-      productId: 1,
-      productName: "Leaf Rake",
-      productCode: "GDN-0011",
-      releaseDate: "March 19, 2019",
-      description: "Leaf rake with 48-inch wooden handle.",
-      price: 19.95,
-      starRating: 3.2,
-      imageUrl: "assets/images/leaf_rake.png"
-    },
-    {
-      productId: 2,
-      productName: "Garden Cart",
-      productCode: "GDN-0023",
-      releaseDate: "March 18, 2019",
-      description: "15 gallon capacity rolling garden cart",
-      price: 32.99,
-      starRating: 4.2,
-      imageUrl: "assets/images/garden_cart.png"
-    }
-  ];
-  constructor() {
-    this.filteredProducts = this.products;
-    this.listFilter = '';
-  }
+  products: IProduct[];
+
   toggleImage(): void {
     this.showImage = !this.showImage;
   }
+
   performFilter(filterBy: string): IProduct[] {
     filterBy = filterBy.toLocaleLowerCase();
     return this.products.filter((product: IProduct) =>
     product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
   }
+
   onRatingClicked(message: string): void {
     this.pageTitle = 'Product List: ' + message;
   }
+  
   ngOnInit(): void {
+    this.productService.getProduct().subscribe(
+      data => {
+        this.products = data;
+        this.filteredProducts = this.products;
+      },
+      error => this.errorMessage = error as any
+    );
   }
 }
