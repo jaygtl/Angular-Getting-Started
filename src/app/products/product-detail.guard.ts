b40365e8186c4018ee321e4cd21989e4e6d1dd80
Jay Rao <jay.rao@gatewaytechnolabs.com>
2019-08-21 22:13:57 +0200

    finishing using feature and shared module

--- src/app/products/product-detail.guard.ts
+++ src/app/products/product-detail.guard.ts
@@ -8,19 +8,18 @@ import { Observable } from 'rxjs';
 export class ProductDetailGuard implements CanActivate {
   constructor(private router: Router){}
 
-  canActivate(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot) : Observable<boolean> | Promise<boolean> | boolean {
-      const id = +next.url[1].path;
-      if(isNaN(id) || id < 0)
-      {
-        alert('Invalid ID');
-        this.router.navigate(['/products']);
-        return false;
-      }
-      else 
-      {
-        return true;
-      }
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean 
+  {
+    const id = +next.url[1].path;
+    if (isNaN(id) || id < 0)
+    {
+      alert('Invalid ID');
+      this.router.navigate(['/products']);
+      return false;
     }
+    else 
+    {
+      return true;
+    }
+  }
 }
